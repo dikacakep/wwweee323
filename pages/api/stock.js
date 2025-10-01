508066dedb6ef8f441485d6dc9a4ceb79ad5572b
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-10-01 22:13:19 +0700

    Update stock.js

--- pages/api/stock.js
+++ pages/api/stock.js
@@ -1,7 +1,7 @@
 // Serverless function untuk mengambil data dari API eksternal
 export default async function handler(req, res) {
   try {
-    const apiUrl = 'http://128.199.212.210:5000/data/stock.json';
+    const apiUrl = 'http://188.166.234.88:3000/stock.json';
     const response = await fetch(apiUrl, {
       method: 'GET',
       headers: {
@@ -23,3 +23,4 @@ export default async function handler(req, res) {
   }
 
 }
+
