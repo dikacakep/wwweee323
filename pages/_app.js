6c981714c80d0df1620363fbc6ec098b1d98bb53
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-10-17 00:56:53 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,6 @@
-import { useEffect } from "react";
+// pages/_app.js
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const script = document.createElement("script");
-      script.src =
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2348732504828730";
-      script.async = true;
-      script.crossOrigin = "anonymous";
-      document.head.appendChild(script);
-      return () => {
-        if (document.head.contains(script)) {
-          document.head.removeChild(script);
-        }
-      };
-    }
-  }, []);
-
   return <Component {...pageProps} />;
 }
